593b908e8c30003ef69a9c0e809e5c3d0aa6a598
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-12-21 20:09:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(str){
+    str = str.replace(/[^a-z0-9áéíóúñü' \.,_-]/gim,"");
+    return str.trim()
+  }
+
+  static titleize(str){
+    let arr = str.split(" ")
+    let newArr =[]
+    console.log(arr)
+    arr.map(x => {
+      if (x==='the'||'a'||'an' || 'but' || 'of' || 'and' || 'for' || 'at' || 'by' || 'from'){
+        newArr.push(x)
+      }
+      else {
+        newArr.push(Formatter.capitalize(x))
+      }
+      let g = newArr.join(" ")
+      let finarr = Formatter.capitalize(g)
+      return finarr
+    })
+  }
 }
